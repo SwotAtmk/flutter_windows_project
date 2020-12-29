1db721ab5984bf20cf9add370fe61b7dfe8ce54f
Jarming <2361288220@qq.com>
2020-12-29 13:59:57 +0800

    fix

--- lib/routes/pages_register.dart
+++ lib/routes/pages_register.dart
@@ -1,6 +1,11 @@
 /// 页面注册，统一管理页面
 
-export '../pages/home.dart';
-export '../pages/test_page.dart';
-export '../pages/to_name_route_test.dart';
 export '../pages/index_page.dart';
+
+
+
+
+/// 测试页不参与主要业务逻辑
+export '../test/home.dart';
+export '../test/test_page.dart';
+export '../test/to_name_route_test.dart';
