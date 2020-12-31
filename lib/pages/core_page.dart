2837f953a19a9cddd7a12f248f210b08d7b4718a
Jarming <2361288220@qq.com>
2020-12-31 17:56:52 +0800

    降重核心功能页面开始

--- lib/pages/core_page.dart
+++ lib/pages/core_page.dart
@@ -1,1 +1,13 @@
 /// 主要降重功能页
+///
+import 'package:flutter/material.dart';
+
+class CorePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.center,
+      child: Text("降重页"),
+    );
+  }
+}
