e9f9e56bdcaf57a273ae3fa6bffe7d6f96b03148
Jarming <2361288220@qq.com>
2021-01-01 15:48:04 +0800

    1、应用信息 2、Dio下载文件与执行文件

--- lib/pages/core_page.dart
+++ lib/pages/core_page.dart
@@ -1,12 +1,36 @@
 /// 主要降重功能页
 import 'package:flutter/material.dart';
+import 'package:win32/win32.dart';
+import 'dart:ffi';
+import '../service/service_register.dart' show DioService;
+import 'package:package_info_plus/package_info_plus.dart';
 
 class CorePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
       alignment: Alignment.center,
-      child: Text("降重页"),
+      child: Column(
+        children: [
+          InkWell(
+            onTap: (){
+              DioService().downloadFile("https://dl.google.com/tag/s/appguid%3D%7B8A69D345-D564-463C-AFF1-A69D9E530F96%7D%26iid%3D%7B0A576E00-3665-36D9-E00F-A3A0DAAC905A%7D%26lang%3Dzh-CN%26browser%3D5%26usagestats%3D1%26appname%3DGoogle%2520Chrome%26needsadmin%3Dprefers%26ap%3Dx64-stable-statsdef_1%26installdataindex%3Dempty/update2/installers/ChromeSetup.exe", "./update/ChromeSetup.exe")
+                  .then((value){
+                    ShellExecute(0,TEXT("open"),TEXT("ChromeSetup.exe"),nullptr,TEXT("./update/"),SW_SHOW);
+                });
+            },
+            child: Text("下载并执行文件"),
+          ),InkWell(
+            onTap: () async {
+              PackageInfo packageInfo = await PackageInfo.fromPlatform();
+              print(packageInfo.appName);
+              print(packageInfo.packageName);
+              print(packageInfo.version);
+            },
+            child: Text("获取应用信息"),
+          )
+        ],
+      )
     );
   }
 }
--- lib/service/dio_service.dart
+++ lib/service/dio_service.dart
@@ -3,13 +3,15 @@ import 'package:dio/dio.dart';
 import 'dart:async';
 import 'package:get/get.dart' hide Response;
 
-class DioService extends GetxService{
+class DioService extends GetxService {
   static Dio dio = new Dio();
 
   ///初始化Dio模块
   Future<DioService> init({isIgnoreCertificate = true}) async {
-    if (isIgnoreCertificate) {///忽略SSL证书错误
-      (dio.httpClientAdapter as DefaultHttpClientAdapter).onHttpClientCreate = (client) {
+    if (isIgnoreCertificate) {
+      ///忽略SSL证书错误
+      (dio.httpClientAdapter as DefaultHttpClientAdapter).onHttpClientCreate =
+          (client) {
         client.badCertificateCallback = (cert, host, port) {
           return true;
         };
@@ -18,21 +20,24 @@ class DioService extends GetxService{
     return this;
   }
 
-  void onInit(){
+  void onInit() {
     super.onInit();
     print("dio service init……");
   }
-  void onReady(){
+
+  void onReady() {
     super.onReady();
     print("dio service onReady……");
   }
-  void onClose(){
+
+  void onClose() {
     super.onClose();
     print("dio service onClose……");
   }
 
   ///请求接口数据
-  Future requestData(path,{param,String contentType="application/x-www-form-urlencoded"}) async {
+  Future requestData(path,
+      {param, String contentType = "application/x-www-form-urlencoded"}) async {
     try {
       print("获取数据${path}.............................");
       dio.options.contentType = contentType;
@@ -47,4 +52,12 @@ class DioService extends GetxService{
     }
   }
 
+  Future<Response> downloadFile(url, savePath) async {
+    return await dio.download(url, savePath,
+        onReceiveProgress: (received, total) {
+      if (total != -1) {
+        print((received / total * 100).toStringAsFixed(0) + "%");
+      }
+    });
+  }
 }
