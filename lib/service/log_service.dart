5c113ee6a862fd0cbb98993ca0c599b3e3ff4b2c
Jarming <2361288220@qq.com>
2021-01-03 22:32:42 +0800

    fix

---
+++ lib/service/log_service.dart
@@ -0,0 +1,11 @@
+import 'dart:io';
+
+class LogService{
+
+  static const OPERATION_LOG = "operation.log";  /// 操作日志
+
+  static const ERROR_LOG = "error.log";          /// 错误
+
+  static const WARN_LOG = "warn.log";            /// 警告
+
+}
